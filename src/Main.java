c55246e1668a42e0fc9db3a8353c2b7af8d2b2db
4804W29 <4804W29@usa.miracosta.cc.ca.us>
2022-08-30 21:43:13 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    /**
+     * (String name, int level, int healthPoints, int gold, int magic)
+     *
+     * 2nd constructor (String name, int level) {
+     *     mName = name;
+     *     mLevel = level;
+     *     mHealthPoints = level * 100;
+     *     mGold = level * 100;
+     *     mMagic = level * 100;
+     * }
+     *
+     * (String name) {
+     *     this(name, 1);
+     * }
+     *
+     *
+     * Child using super(name);
+     *
+     *
+     * @Override    - toString () Redefining a method that came from the parent class, method from the parent class is being redefined, overwritten in the child class?
+     *
+     * @OverLoading - 2 variations total GameCharacter (String name) vs GameCharacter (String name, int level)
+     *              - Same name, different parameters
+     *
+     *
+     * Class name @ hexadecimal address GameCharacter@aed1f14
+     *
+     * @Override is an annotation, but Java recognizes
+     *
+     *
+     * private, protected & public
+     *
+     * private: only the class
+     * protected: class itself and the children (inheritance = protected)
+     * public: everybody had access
+     *
+     *
+     * abstract class cannot be instantiated (no objects, not real)
+     * // no public constructor method
+     * // used for inheritance purposes only, put all common code in parent
+     *
+     *
+     * Main -> psvm public static void main(String args)
+     *
+     *
+     *
+     * Syntax of how to read the parameters
+     */
+}
